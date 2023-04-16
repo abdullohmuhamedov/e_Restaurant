3d18b7a692a2f733eef6f5cd47b9a8e81b502f21
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-04-16 22:53:48 +0500

    little change made

--- e_Restaurant.DAL/Configurations/DbContextApp.cs
+++ e_Restaurant.DAL/Configurations/DbContextApp.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace e_Restaurant.DAL.Configurations;
 
-namespace e_Restaurant.DAL.Configurations
-{
-    internal class DbContextApp
+public class DbContextApp : DbContext
     {
-    }
+
 }
